519e9bfcc5d4321251284ad4c4de279fc6cf05ef
Alex Cross <108724547+alexmcross18@users.noreply.github.com>
2026-03-31 09:54:23 +0100

    Create .txt-file-parsing.py

---
+++ scripts/log-parsing/.txt-file-parsing.py
@@ -0,0 +1,39 @@
+# Imports the json module
+import json
+
+# Creates a variable called "log_lines" and assigns the input file (file with the logs) to it
+log_lines = "logs.txt"
+
+# Opens the logs file, in read mode and assigns the "lines" variable to read each line of the logs file
+with open(log_lines, "r") as input_file:
+    lines = input_file.readlines()
+
+# Creates an empty list to add to later
+parsed_logs = []
+
+# Creates a for loop that removes the new line character "\n" (.strip()) from the end of each line
+# Splits each line on every space (.split(" ")) a maximum of 5 times
+for line in lines:
+    # Try/Except to handle error handling, if there is less splits or malformed/missing text in the logs file it skips
+    # that line, moves onto the next and prints an error message instead of crashing the script
+    try:
+        split_logs = line.strip().split(" ", 4)
+
+        # Creates a dictionary called "log_entries", makes multiple key+value pairs (e.g. key=date value=split_logs[0])
+        log_entries = {
+            "date": split_logs[0],
+            "time": split_logs[1],
+            "level": split_logs[2],
+            "source": split_logs[3],
+            "message": split_logs[4]
+        }
+
+        # Adds the completed dictionary for this line to the parsed_logs list
+        parsed_logs.append(log_entries)
+    except IndexError:
+        print(f"Skipping missing/malformed line:  {line.strip()}")
+
+# Creates a new json file "parsed_logs.json" in write mode and adds the data from the "parsed_logs" list
+# Indents the json file 4 times per row of data to make it easier to read
+with open("parsed_logs.json", "w") as output_file:
+    json.dump(parsed_logs, output_file, indent=4)
